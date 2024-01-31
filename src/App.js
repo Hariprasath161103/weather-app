5b9e1c924f3338ea44df403260e8f92b9043536c
Hariprasath161103 <hariprasaths1611@gmail.com>
2024-01-31 21:08:17 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios"
+import { useState } from "react"
 function App() {
+
+
+  const [degree, setdegree] = useState("0")
+  const [city, setcity] = useState("france")
+  const [weather, setweather] = useState("rainy")
+  const [inputvalue, setinputvalue] = useState()
+
+  function handleinput(event) {
+    setinputvalue(event.target.value)
+  }
+
+  function getdata() {
+    var weather = axios(`https://api.openweathermap.org/data/2.5/weather?q=${inputvalue}&appid=0f4978c6281679611eb050a4d9632e10`)
+    weather.then(function (dalta) {
+      console.log(dalta.data)
+      setdegree(dalta.data.main.temp)
+      setcity(dalta.data.name)
+      setweather(dalta.data.weather[0].main)
+    }).catch(function () {
+      console.log("enter valid city")
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="flex flex-row justify-center h-[100vh] items-center" >
+      <div style={{ backgroundImage: "linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)" }} className="p-2 rounded shadow">
+        <h2 className="font-medium">Hey! ⛅</h2>
+        <p className="text-xs">Do you want to know the weather Report :)</p>
+        <input onChange={handleinput} type="text" className="rounded-md h-6 text-sm mt-2 p-4 outline-none" placeholder="City Name ?"></input>
+        <br></br>
+        <button onClick={getdata} className="bg-black text-white rounded-lg p-2 text-xs mt-2" >Get Report ⚡</button>
+
+        <p className="text-xs mt-2">Degree: {degree} | City: {city} | Weather: {weather}</p>
+      </div>
+    </div>)
 }
 
-export default App;
+export default App
